010369de3c0ed726cfdc62df589bdd6c1da1e286
HALOCORE <WBPROSCI@outlook.com>
2018-12-05 20:41:04 +0800

    update gz_writer

--- gz_writer.py
+++ gz_writer.py
@@ -10,10 +10,16 @@ class GzDigraphWriter:
         self.name = name
         self.__code_list = list()
         self.__code_list.append("digraph " + name + "{")
+        self.__code_list.append("ordering = in;")
 
     def add_edge(self, name1, name2):
         """在digraph上添加边"""
         self.__code_list.append(name1 + " -> " + name2 + ";")
+
+    def set_node(self, name, color="black"):
+        """在digraph中设置节点颜色"""
+        code = name + " [color=" + color + "];"
+        self.__code_list.append(code)
 
     def get_codes(self):
         """获取代码"""
